7522f60d90f4052de3586c5b68b68a8b0d347770
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-08 23:23:16 +0300

    add test for empty string using <conditional <text.length === 0>.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+// Business logic
+
+functionwordCounter(text) {
+  if (text.length === 0) {
+    return 0;
+  }
+}
 function wordcounter(text) {
   let wordCount = 0;
   const wordArray = text.split(" ");
